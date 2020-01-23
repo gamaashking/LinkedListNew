6478433249c4984de8df42e70072498fb49c4c82
gamaashking <ashutosh.shukla_cs18@gla.ac.in>
2020-01-23 05:30:23 +0000

    create the inner class Node<E>

--- src/definition/OurLinkedList.java
+++ src/definition/OurLinkedList.java
@@ -1,4 +1,15 @@
 package definition;
 
-public class OurLinkedList<E> {
+import adt.LinkedListADT;
+
+public class OurLinkedList<E> implements LinkedListADT<E> {
+    @Override
+    public void add(int index, E item) {
+
+
+    }
+
+    private static class Node {
+
+    }
 }
